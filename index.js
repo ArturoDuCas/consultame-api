85341c28e50bfc56cc81409f5d077594b8ae56bd
ArturoDuCas <a00833516@tec.mx>
2023-10-10 09:35:24 -0600

    feat: connection with database

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const pg = require("pg");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const app = express();
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+})
 
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
+})
+
+app.get("/ping", async (req, res) => {
+  const result = await pool.query("SELECT NOW()")
+  return res.json(result.rows[0])
 })
 
 
