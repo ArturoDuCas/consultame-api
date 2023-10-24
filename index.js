6dc62db40115e2130d1a3c0e2b476f30a43463c1
ArturoDuCas <a00833516@tec.mx>
2023-10-24 13:48:15 -0600

    feat: configure cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 dotenv.config();
+app.use(cors({ origin: 'http://localhost:5173' }));
 
 const port = process.env.PORT;
 
