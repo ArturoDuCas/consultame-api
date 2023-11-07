5e1c122511a3ab0d36ee201e0435e4506ea2371e
ArturoDuCas <a00833516@tec.mx>
2023-11-07 15:21:24 -0600

    feat: send room id to mobile app

--- sockets/socketEvents.js
+++ sockets/socketEvents.js
@@ -1,4 +1,8 @@
 module.exports = {
+  IOS_CONNECTION: 'IOS Connection',
+  ROOM_CREATION: 'Room Creation',
+
+
   NODEJS_SERVER_PORT: 'NodeJS Server Port',
   IOS_CLIENT_PORT: 'iOS Client Port'
 };
--- sockets/socketHandlers.js
+++ sockets/socketHandlers.js
@@ -1,12 +1,28 @@
+const { v4: uuidv4 } = require('uuid');
+
 const {
+  IOS_CONNECTION,
+  ROOM_CREATION,
+
+
   NODEJS_SERVER_PORT,
   IOS_CLIENT_PORT,
 } = require('./socketEvents');
 
+
+
 const registerSocketHandlers = (io, socket) => {
-  socket.on(NODEJS_SERVER_PORT, (data) => {
-    console.log(data);
-    io.emit(IOS_CLIENT_PORT, { msg: "Hello from NodeJS Server" });
+  socket.on(IOS_CONNECTION, () => {
+    console.log(`iOS Client connected: ${socket.id}`);
+    // generar una clave unica para el room
+    const uniqueRoomKey = uuidv4();
+    // unir al socket al room
+    socket.join(uniqueRoomKey);
+    // enviar un mensaje al cliente iOS con la clave del room
+    io.to(socket.id).emit(ROOM_CREATION, { roomKey: uniqueRoomKey });
+
+
+    console.log(`IOS Client ${socket.id} joined room ${uniqueRoomKey}`);
   });
 
   // Aquí puedes agregar más manejadores de eventos según sea necesario
