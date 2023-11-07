a9e324368b528b60b14a5bbc9a1ef295dc21270e
ArturoDuCas <a00833516@tec.mx>
2023-11-06 21:08:35 -0600

    feat: create web socket server

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const http = require("http");
+const { Server } = require("socket.io");
+
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 dotenv.config();
@@ -19,7 +25,27 @@ app.get("/", (req, res) => {
 app.use(require("./routes/routes"));
 
 
+// Socket.io
+connections = [];
+io.on('connection', (socket) => {
+  connections.push(socket);
+  console.log(`Connected: ${connections.length} sockets connected`)
 
+  // Disconnect
+  socket.on('disconnect', () => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`Disconnected: ${connections.length} sockets connected`);
+  });
+
+  socket.on("NodeJS Server Port", (data) => {
+    console.log(data);
+    io.emit("iOS Client Port", {
+      msg: "Hello from NodeJS Server"
+    });
+  });
+
+
+});
 
 
 app.listen(port, () => {
