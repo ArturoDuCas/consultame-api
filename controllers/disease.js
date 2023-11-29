6f9abe17f0d7d0f21b3ccafdd6ba9df3bc731b49
ArturoDuCas <a00833516@tec.mx>
2023-11-29 00:21:22 -0600

    get /disease/chronics

--- controllers/disease.js
+++ controllers/disease.js
@@ -2,6 +2,18 @@ const { prisma } = require('../config/db');
 
 
 module.exports = {
+  getChronics: async (req, res) => {
+    try {
+      const diseases = await prisma.disease.findMany({
+        where: {
+          type_id: 1
+        }
+      });
+      res.status(200).json(diseases);
+    } catch(err) {
+      res.status(500).json({message: "Error al obtener las enfermedades crónicas", error: err});
+    }
+  }
 
 
 }
